9928e63cfb19f7baa116d5a8c1c6e87e9227693d
Thomas-Pennisi <thomasbpennisi@hotmail.com>
2025-09-29 13:04:47 +1000

    Refactor scores program

--- prac_02/scores.py
+++ prac_02/scores.py
@@ -2,12 +2,34 @@
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+# score = float(input("Enter score: "))
+# if score < 0 or score > 100:
+#     print("Invalid score")
+# elif score >= 90:
+#     print("Excellent")
+# elif score >= 50:
+#     print("Passable")
+# else:
+#     print("Bad")
+
+import random
+
+def main():
+    score = float(input("Enter score: "))
+    print(calculate_result(score))
+
+    random_score= random.randint(0,100)
+    print(f"Random score: {random_score}")
+    print(calculate_result(random_score))
+
+def calculate_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()
