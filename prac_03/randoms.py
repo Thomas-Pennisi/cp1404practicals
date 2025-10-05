743a212c421e62178076933eb95abb896e546640
Thomas-Pennisi <thomasbpennisi@hotmail.com>
2025-10-05 19:54:09 +1000

    Add code to randoms program

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+# What did you see on line 1? 18
+# 5 and 20
+# What did you see on line 2? 7
+# 3 and 9, can't produce a 4 as it goes up in increments of 2
+#
+# What did you see on line 3? 3.992994080828306
+# 2.5 and 5.5
+
+
+import random
+
+random_number = random.randint(1, 100)
+print("Your random number is:", random_number)
